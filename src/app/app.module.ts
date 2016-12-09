e9bbd0bd080c26315b1b9ebf43bc7952fddd23c7
ugurdonmez <ugurdonmez87@gmail.com>
2016-12-09 22:12:01 +0100

    Initialize ionic project with sigle login page.

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { LoginPage } from '../pages/login/login';
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    LoginPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    LoginPage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
